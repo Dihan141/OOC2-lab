08012a64a8fc7a8afd142359ef964507eb934037
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2022-10-03 02:58:20 +0600

    Update ShapeList.java

--- 200042149_Lab 4/200042149_Lab 4/Shape_problem/src/main/java/ShapeList.java
+++ 200042149_Lab 4/200042149_Lab 4/Shape_problem/src/main/java/ShapeList.java
@@ -7,7 +7,7 @@ public class ShapeList{
 
         for(int i=0; i<objects.length; i++)
         {
-            objects[i].printShape();
+            objects[i].PrintShape();
             shapes += "\n";
         }
         return shapes;
